53ec31b5f8a9ce8d6c3a5171c360325797d4eca7
Tubliy <136933579+Tubliy@users.noreply.github.com>
2024-11-11 17:55:28 -0500

    Update qwotuhbot.py

--- qwotuhbot.py
+++ qwotuhbot.py
@@ -513,13 +513,7 @@ async def help(ctx):
     
     # Send the embed with help information
     await ctx.send(embed=embed)
-    
-@bot.command()
-@commands.is_owner()
-async def stop(ctx):
-    await ctx.send("Stopping the bot and service...")
-    await bot.close()
-    os.system("sudo systemctl stop qwotuhbot.service")
+
     
 # Run the bot with your token
 bot.run('MTI5NzA2MDU0OTIwNDUxMjgzOQ.GfV15z._yiVFaa2DF83VRld8V6EXdQuwHN72DComXJKuo')
