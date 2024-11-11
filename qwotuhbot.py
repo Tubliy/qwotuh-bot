a478efe4914af037288d00c817dbabd759718059
Tubliy <136933579+Tubliy@users.noreply.github.com>
2024-11-11 04:48:02 -0500

    Update qwotuhbot.py

--- qwotuhbot.py
+++ qwotuhbot.py
@@ -224,7 +224,7 @@ def sync_check_twitch_live():
 
         # Set up Chrome options
         chrome_options = Options()
-       chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
